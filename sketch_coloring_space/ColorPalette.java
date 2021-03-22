a777ffd2977e20b2f1aaf333bdb75ed32f4ae9e3
Linnea Kirby <linneakirby@gmail.com>
2021-03-22 12:25:09 -0400

    create structure for modular color palette

---
+++ sketch_coloring_space/ColorPalette.java
@@ -0,0 +1,39 @@
+public class ColorPalette{
+  public ColorSwatch[] palette = new ColorSwatch[6];
+  
+  public ColorPalette(ColorSwatch[] p){
+    for(int i=0; i<p.length; i++){
+      this.palette[i] = p[i];
+    }
+  }
+  
+  //returns black if swatch doesn't exist
+  public ColorSwatch getSwatch(int index){
+    if(this.palette[index]){
+      return this.palette[index];
+    }
+    return new ColorSwatch(0, 0, 0);
+  }
+  
+  //returns previous color or black if no previous color  
+  public void setSwatch(int index, ColorSwatch swatch){
+    ColorSwatch ret = this.getSwatch(index);
+    this.palette[index] = swatch;
+    return ret;
+  }
+  
+  //returns previous color or black if no previous color
+  public ColorSwatch addSwatch(ColorSwatch swatch){
+    ColorSwatch ret = this.getSwatch(this.palette.length);
+    this.palette[this.palette.length] = swatch;
+    return ret;
+  }
+  
+  //removes swatch at index and replaces with black, returns original color or black if no original color
+  public ColorSwatch removeSwatch(int index){
+    ColorSwatch ret = this.getSwatch(index);
+    this.palette[index] = new ColorSwatch(0, 0, 0);
+    return ret;
+  }
+  
+}
---
+++ sketch_coloring_space/ColorSwatch.java
@@ -0,0 +1,34 @@
+public class ColorSwatch{
+  private int r;
+  private int g;
+  private int b;
+  
+  public ColorSwatch(int r, int g, int b){
+    this.r = r;
+    this.g = g;
+    this.b = b;
+  }
+  
+  public int getRed(){
+    return this.r;
+  }
+  
+  public int getGreen(){
+    return this.g;
+  }
+  
+  public int getBlue(){
+    return this.b;
+  }
+  
+  public int[] getColor(){
+    return {this.r, this.g, this.b};
+  }
+  
+  public void setColor(int[] c){
+    this.r = c[0];
+    this.g = c[1];
+    this.b = c[2];
+  }
+  
+}
